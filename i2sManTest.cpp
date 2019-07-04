f10966c17cd2cd7c8b4a21c8cf413d014d1e3655
UTKARSH RAI <37475612+ur10@users.noreply.github.com>
2019-07-04 06:33:33 +0530

    Attached custom call back function.
    I have tested it with a string, needs to be tested on the actual device.

--- i2sManTest.cpp
+++ i2sManTest.cpp
@@ -14,10 +14,12 @@ extern "C"{
 
 void app_main()
 {
+  uint16_t *data= (uint16_t*)malloc(sizeof(uint16_t)*80)  //buffer of sufficient length
+  uint16_t length;
     soundMan smgr;
      smgr.init();    
-    smgr.attach_transmit_cb(cb_function);
-   
+    smgr.attach_transmit_cb(cb_function);//attaching the custom call back function
+    smgr.tx_audio(&data,length); // txing 
 
     // Tasks:
     // TODO: Define cb function.To do whatever with the data.
@@ -31,8 +33,9 @@ esp_err_t cb_function(uint16_t* ip_buffer,uint16_t len)
     we could test a .wav file but that would require SD card functionality,
     transmitting a simple text msg.
     */
+   len = sizeof(string)*2; 
    ip_buffer = (uint16_t*)&string;
-    len = sizeof(string); 
+    
 }
 
 
